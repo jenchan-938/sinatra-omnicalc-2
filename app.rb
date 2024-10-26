9397d2a98411f14bd9ad337ec1806a740b62a8f4
jenchan-938 <jchan5@uchicago.edu>
2024-10-26 01:53:31 +0000

    8:53

--- app.rb
+++ app.rb
@@ -21,18 +21,24 @@ get("/subtract") do
   erb(:subtraction_form)
 end
 
-get("/wizard_subtract") do
-  @first_num = params.fetch("first_num").to_f
-  @second_num = params.fetch("second_num").to_f
+get("/wizard_subtraction") do
+  @first_num = params.fetch("first_number").to_f
+  @second_num = params.fetch("second_number").to_f
+
+  @result = @second_num - @first_num
 
   erb(:sub_result)
 end
 
 get("/multiply") do
-  erb(:multiplication_form)
+  erb(:mult_form)
 end
 
-get("/wizard multiply") do
+get("/wizard_multiply") do
+  @first_num = params.fetch("first_number").to_f
+  @second_num = params.fetch("second_number").to_f
+  @result = @first_num * @second_num
+
   erb(:mult_result)
 end
 
